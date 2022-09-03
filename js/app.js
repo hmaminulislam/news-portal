203abb973e46f2b8232e21e93247567c47c32e6d
Aminul <mdaminulislamsahin14@gmail.com>
2022-09-03 08:37:13 +0600

    author added

--- js/app.js
+++ js/app.js
@@ -37,10 +37,12 @@ const articleDisplay = (articles) => {
         categoryItemFound.innerText = `${articles.length} items found for category`
     }
     // Article display 
+    console.log(articles)
     const articleContainer = document.getElementById("article-containe");
     articleContainer.textContent = '';
     articles.forEach(article => {
         const {image_url, title, details, author, rating, total_view} = article;
+        const {name, img, published_date} = author;
         const articleRow = document.createElement("div");
         articleRow.classList.add("row", "mt-5", "border", "rounded", "py-3");
         articleRow.innerHTML = `
@@ -51,10 +53,35 @@ const articleDisplay = (articles) => {
             <div class="card-body">
                 <h4 class="card-title">${title}</h4>
                 <p class="card-text mt-3">${details}</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex align-items-center">
+                        <img class="author-img" src=${img}>
+                        <div class="ms-3">
+                            <p class="mb-1 fw-semibold">${name}</p>
+                            <span>${published_date}</span>
+                        </div>
+                    </div>
+                    <div>
+                        <i class="fa-regular fa-eye"></i>
+                        <span class="fw-semibold">${total_view}M</span>
+                    </div>
+                    <div>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <span class="fw-semibold">${rating.number}</span>
+                    </div>
+                    <div>
+                        <button class="btn btn-info text-light">Details</button>
+                    </div>
+                </div>
             </div>
         </div>
         `
         articleContainer.appendChild(articleRow)
     })
 }
+
+articleData(01)
